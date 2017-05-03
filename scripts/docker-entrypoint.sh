6a0df79830b842e812ee3b6bf0f992c45f2897b5
Samael Infernus <Samael Infernus>
2017-05-02 20:50:40 -0400

    mffs

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -12,8 +12,8 @@ if [ "$1" = 'app' ]; then
   secret_key_base=$(ruby -r securerandom -e "puts SecureRandom.hex(64)")
   export secret_key_base
 
-  echo "==> npm install ugh..."
-  npm install
+  echo "===> npm install ugh..."
+  exec npm install
   # echo "===> Running db:schema:load..."
   # bundle exec rake db:schema:load
   echo "===> Running db:structure:load..."
